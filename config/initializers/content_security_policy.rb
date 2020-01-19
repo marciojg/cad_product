55a43fdc86917bf8606e0559d28c12d0e7b22456
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-01-19 10:32:50 -0300

    add frozen_string_literal

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.config.content_security_policy do |policy|
   policy.connect_src :self, :https, 'http://localhost:3009', 'ws://localhost:3009', 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
 end
